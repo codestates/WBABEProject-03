bd13ab4175b21a14f85378d399bda01985abb3ff
김경탁 <tak6831@gmail.com>
2022-12-23 21:08:03 +0900

    feat : add test

---
+++ model/model_test.go
@@ -0,0 +1,40 @@
+package model_test
+
+import (
+	"context"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Model struct {
+	client     *mongo.Client
+	colPersons *mongo.Collection
+}
+
+type Person struct {
+	Name string `bson:"name"`
+	Age  int    `bson:"age"`
+	Pnum string `bson:"pnum"`
+}
+
+func NewModel() (*Model, error) {
+	r := &Model{}
+
+	var err error
+	mgUrl := "mongodb://127.0.0.1:27017"
+	if r.client, err = mongo.Connect(context.Background(), options.Client().ApplyURI(mgUrl)); err != nil {
+		return nil, err
+	} else if err := r.client.Ping(context.Background(), nil); err != nil {
+		return nil, err
+	} else {
+		db := r.client.Database("go-ready")
+		r.colPersons = db.Collection("tPerson")
+	}
+
+	return r, nil
+}
+func Test_db(t *testing.T) {
+	const v = "123"
+}
